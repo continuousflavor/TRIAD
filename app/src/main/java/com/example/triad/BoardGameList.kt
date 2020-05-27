948f83286b9847b85061b78c55ed7bb4cfab4021
Justin Sabatini <justin.sabatini@gmail.com>
2020-05-27 16:15:00 -0500

    First commit

---
+++ app/src/main/java/com/example/triad/BoardGameList.kt
@@ -0,0 +1,12 @@
+package com.example.triad
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BoardGameList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_game_list)
+    }
+}
---
+++ app/src/main/java/com/example/triad/BoardGameMenu.kt
@@ -0,0 +1,21 @@
+package com.example.triad;
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_board_game_menu.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class BoardGameMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_game_menu)
+
+      button2.setOnClickListener{
+          val intent = Intent(this,BoardGameList::class.java)
+            startActivity(intent)
+      }
+
+    }
+}
