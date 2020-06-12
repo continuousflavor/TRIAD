75b387c4b1436a1593a7a02d9df3300da899b339
Justin Sabatini <justin.sabatini@gmail.com>
2020-06-11 21:56:04 -0500

    updated graphics

--- app/src/main/java/com/example/triad/BoardGameList.kt
+++ app/src/main/java/com/example/triad/BoardGameList.kt
@@ -8,5 +8,12 @@ class BoardGameList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_game_list)
+
+
+
+
     }
+
+
+
 }
