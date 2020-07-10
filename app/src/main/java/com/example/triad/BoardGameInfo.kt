d0ed994972847a41b2de5404b6288f227d8c2733
Justin Sabatini <justin.sabatini@gmail.com>
2020-07-10 15:34:41 -0500

    added gameForId

--- app/src/main/java/com/example/triad/BoardGameInfo.kt
+++ app/src/main/java/com/example/triad/BoardGameInfo.kt
@@ -1,37 +1,46 @@
 package com.example.triad
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_board_game_info.*
+
 
 class BoardGameInfo  : AppCompatActivity() {
 
-    private lateinit var gameAdapter: GameItemRecyclerAdapter
+//    private lateinit var gameAdapter: GameItemRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // todo: have this load a view with no recycler, just a linearlayout or card view
         setContentView(R.layout.activity_board_game_info)
 
-        initRecyclerView()
-        addDataSet()
+//        initRecyclerView()
+        // find the ID of the item to view
+        val intent = intent
+        val uri = intent.data
+
+        // TODO: fix this so that we don't crash if passed a bad (or no) URI
+        val gameId = uri!!.path!!.toInt()
+
+        addDataSet(gameId)
 
     }
 
-    private fun addDataSet(){
-        val data = DataSource.createDataSet()
-        gameAdapter.submitList(data)
+    private fun addDataSet(gameId: Int){
+        val game = DataSource.gameForId(gameId)
+
+        // todo: set this data on the card to display
+//        gameAdapter.submitList(data)
     }
 
-    private fun initRecyclerView(){
-        game_item_recycler_view.apply {
-            layoutManager = LinearLayoutManager(this@BoardGameInfo)
-            val topSpacingItemDecoration = TopSpacingItemDecoration(20)
-            addItemDecoration(topSpacingItemDecoration)
-            gameAdapter = GameItemRecyclerAdapter()
-            adapter = gameAdapter
-
-        }
-    }
+//    private fun initRecyclerView(){
+//        game_item_recycler_view.apply {
+//            layoutManager = LinearLayoutManager(this@BoardGameInfo)
+//            val topSpacingItemDecoration = TopSpacingItemDecoration(20)
+//            addItemDecoration(topSpacingItemDecoration)
+//            gameAdapter = GameItemRecyclerAdapter()
+//            adapter = gameAdapter
+//
+//        }
+//    }
 }
--- app/src/main/java/com/example/triad/DataSource.kt
+++ app/src/main/java/com/example/triad/DataSource.kt
@@ -6,7 +6,28 @@ class DataSource {
 
     companion object{
 
-        fun createDataSet(): ArrayList<GameList>{
+        fun gameForId(gameId: Int): GameList? {
+            // todo: look game up by ID instead of returning a hardcoded game
+            // should call createDataSet and loop over the results and return the first one that matches
+            return GameList(
+                1,
+                "Lords of Waterdeep",
+                "Fantasy worker placement",
+                "https://raw.githubusercontent.com/continuousflavor/TRIAD/master/lords_of_waterdeep.png",
+                "5/5 stars (378)",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel orci porta non pulvinar. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Et pharetra pharetra massa massa ultricies. Ultricies tristique nulla aliquet enim tortor. In nibh mauris cursus mattis. Adipiscing elit duis tristique sollicitudin nibh sit. Dui sapien eget mi proin. Tortor id aliquet lectus proin nibh nisl condimentum id. Ullamcorper malesuada proin libero nunc consequat. Et leo duis ut diam quam nulla. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Sodales ut etiam sit amet. Neque viverra justo nec ultrices dui. Diam ut venenatis tellus in metus vulputate eu scelerisque. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. Dictum varius duis at consectetur lorem. Lectus nulla at volutpat diam ut venenatis tellus. At urna condimentum mattis pellentesque id nibh. Molestie a iaculis at erat pellentesque adipiscing commodo elit at.\n" +
+                        "\n" +
+                        "Nunc sed id semper risus in hendrerit gravida rutrum. Elit at imperdiet dui accumsan sit amet nulla facilisi morbi. Leo integer malesuada nunc vel risus commodo viverra. Quis auctor elit sed vulputate mi sit amet mauris. Id velit ut tortor pretium viverra. Imperdiet dui accumsan sit amet nulla facilisi morbi. Sapien et ligula ullamcorper malesuada proin libero nunc. Ipsum dolor sit amet consectetur adipiscing elit ut. Felis bibendum ut tristique et egestas quis ipsum. Odio euismod lacinia at quis risus sed.\n" +
+                        "\n" +
+                        "Fringilla ut morbi tincidunt augue interdum velit. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Sed egestas egestas fringilla phasellus. Lectus magna fringilla urna porttitor rhoncus dolor. Convallis a cras semper auctor neque vitae tempus quam. Vitae et leo duis ut diam quam nulla porttitor. Eget nunc scelerisque viverra mauris. Ornare suspendisse sed nisi lacus sed viverra tellus in hac. Nullam eget felis eget nunc lobortis. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus. Tempus egestas sed sed risus pretium. Amet tellus cras adipiscing enim eu turpis egestas. Tellus orci ac auctor augue.\n" +
+                        "\n" +
+                        "Venenatis cras sed felis eget velit aliquet sagittis. Aliquam etiam erat velit scelerisque in dictum non consectetur a. Mattis pellentesque id nibh tortor id aliquet. Ipsum faucibus vitae aliquet nec ullamcorper sit. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi. Scelerisque in dictum non consectetur a. Faucibus ornare suspendisse sed nisi. Lacus viverra vitae congue eu consequat ac felis. Ac placerat vestibulum lectus mauris ultrices eros in cursus turpis. Id leo in vitae turpis massa sed elementum tempus egestas. Vel turpis nunc eget lorem. Libero volutpat sed cras ornare. Nec sagittis aliquam malesuada bibendum arcu. Mauris augue neque gravida in fermentum et sollicitudin. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Tristique et egestas quis ipsum suspendisse ultrices. Pharetra pharetra massa massa ultricies mi. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque."
+
+            )
+        }
+
+
+            fun createDataSet(): ArrayList<GameList>{
             val list = ArrayList<GameList>()
             list.add(
                 GameList(
